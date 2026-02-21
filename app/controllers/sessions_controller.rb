95baf079649d07f1d45d24b2d0c71f2118cf5799
mohamed <mohamed.mokaddem@proton.me>
2026-02-21 15:09:41 +0100

    Add skip_before_action for verify_authenticity_token in SessionsController#create to allow API requests without CSRF token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :require_user, only: [:create, :destroy]
+  skip_before_action :verify_authenticity_token, only: [:create]
 
     def create
         user = User.find_by(username: params[:session][:username].downcase)
