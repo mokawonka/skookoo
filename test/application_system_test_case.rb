6a773abe388848bdb7a97311c50bb2f3106e2b6a
mohamed <mohamed.mokaddem@proton.me>
2026-02-28 20:19:01 +0100

    tests bug fix

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,9 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  
+  # Fix ChromeDriver version compatibility for GitHub Actions
+  before_setup do
+    Webdrivers::Chromedriver.required_version = "120.0.6099.109"
+  end
 end
