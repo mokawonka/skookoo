ced19a425688af6e4be5b3214620078e4f3cae72
mohamed <mohamed.mokaddem@proton.me>
2026-02-28 20:49:52 +0100

    tests bug fix

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,6 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
-  driven_by :selenium, using: :chrome_headless, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 
 end
